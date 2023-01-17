6e4b8372245dac2bea74c16e3d3f28f33ae9bab6
pooranjoyb <pooranjoyb2016@gmail.com>
2023-01-17 21:54:25 +0530

    Built initial UI

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 import express from 'express'
-// import cors from 'cors'
+import bp from'body-parser'
 
 const app = express();
 
-// app.use(cors());
+//setting up viewport for ejs
+app.set('view engine', 'ejs');
 
+//loading static asssets
 app.use(express.static("./public"))
+
+app.use(bp.json());
+app.use(bp.urlencoded({extended: false}))
+
+//home route
+app.get('/', (req, res) => {
+    res.render('../public/views/base', {title: "Hostel Portal"});
+})
 
 app.listen(3000, () => {
     console.log("App listening on port 3000")
